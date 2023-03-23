11f273822c7d17a413ac2c93d2057d966752b902
Marko IT <xBravix@gmail.com>
2023-03-23 14:52:12 +0100

    adding code

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
-# This is a sample Python script.
+import os
+# clear = lambda: os.system('cls')
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def write_empty(file_name):
+    with open(file_name + ".max", "w") as output_file:
+        output_file.write("\n")
 
+while (True):
+    file_name = input("Podaj nazwę pliku: ")
+    try:
+        with open(file_name, "r") as file:
+            file_contents = file.readlines()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+            if not file_contents:
+                write_empty(file_name)
 
+            else:
+                people = []
+                for line in file_contents:
+                    # print(line)
+                    data = line.strip().split()
+                    if len(data) == 4:
+                        people.append(data)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+                if not people:
+                    write_empty(file_name)
+                else:
+                    max_efficiency_people = []
+                    max_efficiency = 0
+                    for person in people:
+                        if len(person[0]) > 3 and person[1].endswith("ski"):
+                            efficiency = float(person[3])
+                            if efficiency > max_efficiency:
+                                max_efficiency_people = [(person, efficiency)]
+                                max_efficiency = efficiency
+                            elif efficiency == max_efficiency:
+                                max_efficiency_people.append((person, efficiency))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+                    if max_efficiency_people:
+                        output_file_name = file_name + ".max"
+                        with open(output_file_name, "w") as output_file:
+                            for person, efficiency in max_efficiency_people:
+                                anonymized_surname = person[1][0] + "*" * (len(person[1]) - 4) + person[1][-3:]
+                                output_file.write(f"{person[0]} {anonymized_surname} {person[2]}\n")
+                    else:
+                        write_empty(file_name)
+        break
+
+    except FileNotFoundError:
+        print(f"Plik '{file_name}' nie istnieje.")
+        # clear()
+    except IOError:
+        print(f"Wystąpił błąd wejścia-wyjścia podczas pracy z plikiem '{file_name}'.")
+        # clear()
